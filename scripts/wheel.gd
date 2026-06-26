3f726eab0d82b977c442746b455d8f13ba00641d
anajshah <anajshah@gmail.com>
2026-06-26 11:32:22 -0700

    this is actual ragebait why does it keep spinning holy moly dude hour 7.5 me things

--- scripts/wheel.gd
+++ scripts/wheel.gd
@@ -16,9 +16,11 @@ func _process(delta: float) -> void:
 		speed -= friction * delta
 		if speed < 0:
 			speed = 0
- 
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var cover = get_node_or_null("../cover_rot")
+		if cover and cover.is_rotated == false:
+			return
 		if speed == 0:
 			speed = randf_range(3.0, 8.0)
