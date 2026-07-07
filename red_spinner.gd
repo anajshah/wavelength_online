0db4cb460c164a7296b1dd9cac46d886240b22a6
anajshah <anajshah@gmail.com>
2026-07-06 19:24:05 -0700

    omg it was so easy to fix i spent like 3 hours and it was some viewport thingy

--- red_spinner.gd
+++ red_spinner.gd
@@ -6,11 +6,20 @@ func _ready() -> void:
 	$Area2D.mouse_entered.connect(_on_mouse_entered)
 	$Area2D.mouse_exited.connect(_on_mouse_exited)
 
-func _process(_delta: float) -> void:
-	if is_hovering and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var mouse_pos = get_global_mouse_position()
+#func _process(_delta: float) -> void:
+	#if is_hovering and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		#var mouse_pos = get_global_mouse_position()
 		
-		global_rotation = global_position.angle_to_point(mouse_pos)
+		#global_rotation = global_position.angle_to_point(mouse_pos)
+		
+		#get_viewport().set_input_as_handled()
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if is_hovering and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var mouse_position = get_global_mouse_position()
+		global_rotation = global_position.angle_to_point(mouse_position)
+		
+		get_viewport().set_input_as_handled()
 
 func _on_mouse_entered() -> void:
 	is_hovering = true
